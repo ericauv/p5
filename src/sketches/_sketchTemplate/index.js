a47ec52fbb82e67536b55e26a65e944aa814a278
ericauv <eriauv17@gmail.com>
2020-12-13 11:10:28 -0500

    update sketch template for new grid args

--- src/sketches/_sketchTemplate/index.js
+++ src/sketches/_sketchTemplate/index.js
@@ -1,11 +1,14 @@
 const height = window.innerHeight;
 const width = window.innerWidth;
-const gridInterval = 10
+const gridIntervalVertical = 10
+const gridIntervalHorizontal = 10
+
 function setup() {
     createCanvas(width, height);
     background(220);
-    Grid(gridInterval);
+    Grid(gridIntervalVertical, gridIntervalHorizontal);
   }
   
   function draw() {
+
   }
