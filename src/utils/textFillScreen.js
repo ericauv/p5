5fd17d42c8020293bdb009676a34bcd675fe71a8
ericauv <eriauv17@gmail.com>
2020-12-13 22:24:31 -0500

    add textFillScreen util

---
+++ src/utils/textFillScreen.js
@@ -0,0 +1,14 @@
+function TextFillScreen(str = 'text', alternateInterval = 0, alternateStyle = () => null, ){
+    textAlign(CENTER,TOP);
+    size = textSize()
+    intervals = height / (size);
+    for(let i=0; i< intervals; i++){
+        if(alternateInterval && i % alternateInterval === 0){
+            push();
+            alternateStyle();
+            text(str, width / 2, i * height / intervals);
+            pop();
+        }
+        text(str, width / 2, i * height / intervals);
+    }
+}
