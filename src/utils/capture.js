5e4cd7663ceb8f698327572b104291904ddb7d36
ericauv <eriauv17@gmail.com>
2020-12-16 17:20:36 -0500

    add ffmpeg command to capture util

--- src/utils/capture.js
+++ src/utils/capture.js
@@ -1,3 +1,11 @@
+/* add the following to index.html
+* <script src="node_modules/ccapture.js/build/CCapture.all.min.js"></script>
+*/
+
+/* ffmpeg command when in folder that contains the png sequence
+* ffmpeg -r 60 -f image2 -i "%07d.png" -vcodec libx264 -crf 17 -pix_fmt yuv420p -vf scale=1080x1080 output.mp4 
+*/
+
 const height = 1080;
 const width = 1080;
 let isCapturing = false;
@@ -11,8 +19,6 @@ const timeLimit = 10;
 const autoSaveTime = timeLimit;
 
 // set up CCapture, make sure to add the following to index.html
-// <script src="node_modules/ccapture.js/build/CCapture.all.min.js"></script>
-
 const capturer = new CCapture({
     verbose: true,
     format,
