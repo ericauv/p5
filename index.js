79113bd0184715eda83f04bd15de051ef261dd4a
ericauv <eriauv17@gmail.com>
2020-12-18 23:29:20 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const height = window.innerHeight;
-const width = window.innerWidth;
+let height;
+let width;
 
 function setup() {
+    height = 1080;
+    width = 1080;
     createCanvas(width, height);
 }
 
 function draw() {
-  }
+  background(255);
+  translate(width / 2, height /2);
+  text('index.js', 0, 0);
+}
